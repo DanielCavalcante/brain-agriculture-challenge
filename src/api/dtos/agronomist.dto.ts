426daa9dccea044e4aa421738732650588030969
Daniel Cavalcante de Souza <danielcdesouza@gmail.com>
2023-05-01 07:51:49 -0300

    fix: 🐛 Remove import unused

--- src/api/dtos/agronomist.dto.ts
+++ src/api/dtos/agronomist.dto.ts
@@ -1,10 +1,4 @@
-import {
-  IsNotEmpty,
-  IsString,
-  Matches,
-  MaxLength,
-  MinLength
-} from 'class-validator';
+import { IsNotEmpty, IsString, Matches, MinLength } from 'class-validator';
 
 const cpfAndCnpjRegex =
   /(^\d{3}\.\d{3}\.\d{3}\-\d{2}$)|(^\d{2}\.\d{3}\.\d{3}\/\d{4}\-\d{2}$)/;
